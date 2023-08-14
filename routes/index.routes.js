5d02ec13138497859491f79a9226d7428170146c
ArvidErik <erik.arvid.hosszu@gmail.com>
2023-08-14 16:06:25 +0200

    index created Co-authored-by: Óscar Mesejo <OscarMesejo@users.noreply.github.com> Co-authored-by: Rodricancio8 <Rodricancio8@users.noreply.github.com>

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,9 +1,27 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 /* GET home page */
 router.get("/", (req, res, next) => {
-  res.render("index");
+  //API connection
+
+  const url =
+    "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1";
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization: process.env.API_AUTH,
+    },
+  };
+
+  fetch(url, options)
+    .then((res) => res.json())
+    .then((json) => {
+      console.log(json.results);
+      res.render("index", { movies: json.results });
+    })
+    .catch((err) => console.error("error:" + err));
 });
 
 module.exports = router;
