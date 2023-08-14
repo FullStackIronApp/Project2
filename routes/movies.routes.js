055dc8226767911acd2963e76a06e87e2dd5e1cc
ArvidErik <erik.arvid.hosszu@gmail.com>
2023-08-14 16:54:20 +0200

    movies improved

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,38 +1,47 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Movie = require('../models/Movie.model');
+const Movie = require("../models/Movie.model");
+const Review = require("../models/Review.model");
+
+router.get("/movies", (req, res) => {
+  Movie.find()
+    .then((movies) => {
+      res.render("movies", { movies });
+    })
+    .catch((error) => console.log(error));
+});
 
 router.get("/movies/create", (req, res) => {
-    res.render('movies.create')
-  })
+  res.render("movies-create");
+});
 
-  router.post('movies/create', async (req, res, next) => {
-    const { title, year, genre, director, movieImg, review, score } = req.body;
-    Review.create({
+router.post("/movies/create", (req, res, next) => {
+  const { title, year, genre, director, movieImg, content, score } = req.body;
 
-            content: review,
-            score: score,
-            createdBy: req.session.currentUser.username // check later if is working after session
-        })
-
-        .then((reviewFromDB)=>{
-            Movie.create({
-                title,
-                year,
-                genre,
-                director,
-                movieImg,
-                reviews: [reviewFromDB._id],
-                score,
-            
-            })
-            .then((res)=>{
-                console.log("Movie created", res)
-                res.redirect('/movies/create');
-            })
-            .catch(err => next(err))
-        })
-        .catch(err => next(err))
+  Review.create({
+    content,
+    score,
+    // createdBy:  req.session.currentUser.username, check later if is working after session
     })
-  
-  module.exports = router;
+    .then((reviewFromDB) => {
+      console.log(reviewFromDB);
+      Movie.create({
+        title,
+        year,
+        genre,
+        director,
+        movieImg,
+        reviews: [reviewFromDB],
+        score,
+      });
+    })
+    .then((result) => {
+      console.log("Movie created", result);
+      res.redirect("/movies");
+    })
+    .catch((err) => next(err));
+});
+
+
+
+module.exports = router;
