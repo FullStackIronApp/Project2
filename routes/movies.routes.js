729fbd17a6d683fc585cf74f65062a200caf668a
ArvidErik <erik.arvid.hosszu@gmail.com>
2023-08-15 12:26:56 +0200

    review done

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const Movie = require("../models/Movie.model");
 const Review = require("../models/Review.model");
 const { isLoggedIn, isLoggedOut } = require("../middleware/route-guard.js");
-const fileUploader = require('../config/cloudinary.config.js');
-
+const fileUploader = require("../config/cloudinary.config.js");
+const { ObjectId } = require("bson");
 
 router.get("/movies", isLoggedIn, (req, res) => {
   Movie.find()
@@ -18,44 +18,82 @@ router.get("/movies/create", isLoggedIn, (req, res) => {
   res.render("movies-create");
 });
 
-router.post("/movies/create", fileUploader.single('movieImg'), isLoggedIn, (req, res, next) => {
-  const { title, year, genre, director, content, score } = req.body;
+router.post(
+  "/movies/create",
+  fileUploader.single("movieImg"),
+  isLoggedIn,
+  (req, res, next) => {
+    const { title, year, genre, director, content, score } = req.body;
 
-  console.log(req.file.path);
+    console.log(req.file.path);
 
-  Review.create({
-    content,
-    score,
-    createdBy: req.session.currentUser._id
-  })
-    .then((reviewFromDB) => {
-      console.log(reviewFromDB);
-      return Movie.create({
-        title,
-        year,
-        genre,
-        director,
-        movieImg: req.file.path,
-        reviews: [reviewFromDB],
-        score,
-        uploadedBy: req.session.currentUser._id,
-      });
+    Review.create({
+      content,
+      score,
+      createdBy: req.session.currentUser.username,
     })
-    .then((result) => {
-      console.log("Movie created", result);
-      res.redirect("/movies");
-    })
-    .catch((err) => next(err));
-});
+      .then((reviewFromDB) => {
+        console.log(reviewFromDB);
+        return Movie.create({
+          title,
+          year,
+          genre,
+          director,
+          movieImg: req.file.path,
+          reviews: [reviewFromDB],
+          score,
+          uploadedBy: req.session.currentUser._id,
+        });
+      })
+      .then((result) => {
+        console.log("Movie created", result);
+        res.redirect("/movies");
+      })
+      .catch((err) => next(err));
+  }
+);
 
 router.get("/movies/:userId", (req, res, next) => {
   const { userId } = req.params;
 
-  Movie.find( {uploadedBy: userId} )
+  Movie.find({ uploadedBy: userId })
     .then((movies) => {
-      res.render("user-movies", {movies})
+      res.render("user-movies", { movies });
     })
     .catch((err) => next(err));
 });
+
+router.get("/movie/:movieId", (req, res, next) => {
+  const { movieId } = req.params;
+
+  Movie.findById(movieId)
+    .populate("uploadedBy")
+    .populate("reviews")
+    .then((movie) => {
+      console.log(movie);
+      res.render("movie-details.hbs", { movie });
+    })
+    .catch((err) => next(err));
+});
+
+router.post("/movie/:movieId", (req,res,next)=>{
+  const movieId = req.params
+  const {content, score} = req.body
+
+  Review.create({
+    content,
+    score,
+    createdBy: req.session.currentUser.username,
+  })
+  .then((result) => {
+    console.log("Review created", result);
+    return Movie.findByIdAndUpdate(movieId.movieId, { $push: { reviews: result._id}}, { new: true })
+  })
+  .then((newMovie) => {
+    console.log("new update", newMovie);
+    res.redirect(`/movie/${newMovie._id}`)
+  })
+  .catch((err) => next(err));
+})
 
 module.exports = router;
